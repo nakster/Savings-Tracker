4a689e567c3ed14136d54d1975fb4e8dedc23869
nakster <g00332403@gmit.ie>
2018-03-31 21:50:32 +0100

    Goal Model Added

---
+++ Savings Tracker/Model/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savings_Tracker.Model
+{
+    //make it public so you can access it 
+    public class Goal
+    {
+        public string Name { get; set; }
+        public decimal SavingGoal { get; set; }
+        public string Notes { get; set; }
+    }
+}
