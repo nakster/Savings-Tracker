154858545cd1bdad1db9216c24d93dc952aa90fa
nakster <g00332403@gmit.ie>
2018-04-01 17:29:41 +0100

    download the 1.5 versions of sqlite and tools added the context file

--- Savings Tracker/DataContext/GoalDataContext.cs
+++ Savings Tracker/DataContext/GoalDataContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Savings_Tracker.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace Savings_Tracker.DataContext
 {
-    public class GoalDataContext
+    public class GoalDataContext : DbContext
     {
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=GoalTracker.db");
+          
+        }
     }
 }
