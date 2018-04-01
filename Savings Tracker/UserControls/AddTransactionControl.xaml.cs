20ab0be45f5b17d684eae40e00298358c05b05a3
nakster <g00332403@gmit.ie>
2018-04-01 18:38:39 +0100

    changes made

--- Savings Tracker/UserControls/AddTransactionControl.xaml.cs
+++ Savings Tracker/UserControls/AddTransactionControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Savings_Tracker.DataContext;
+using Savings_Tracker.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,19 +21,45 @@ namespace Savings_Tracker.UserControls
 {
     public sealed partial class AddTransactionControl : UserControl
     {
+        private int _goalid;
+
         public AddTransactionControl()
         {
             this.InitializeComponent();
         }
 
+        private void SetGoalId(int id)
+        {
+            _goalid = id;
+
+        }
+
         private void ConfirmButton_Click(object sender, RoutedEventArgs e)
         {
+            var NewTransaction = new Transaction();
+            NewTransaction.Date = DateTime.Now;
+            NewTransaction.Amount = Convert.ToInt32(AmountTextBox.Text);
+            NewTransaction.GoalId = _goalid;
 
+            DataContextHelper.AddRecord<Transaction>(NewTransaction);
+            ClearFields();
+            HideControl();
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
+            ClearFields();
 
+        }
+
+        private void ClearFields()
+        {
+            AmountTextBox.Text = string.Empty;
+        }
+
+        private void HideControl()
+        {
+            Visibility = Visibility.Collapsed;
         }
     }
 }
